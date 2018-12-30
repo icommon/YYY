71bb29d2664d0c38d82de40612070e98f534bb06
yuanyi <softwareyuanyi@outlook.com>
2018-12-30 18:19:26 +0800

    delete

---
+++ clanguage/clanguage/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  clanguage
+//
+//  Created by 袁毅 on 2018/12/30.
+//  Copyright © 2018年 yuanyi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
